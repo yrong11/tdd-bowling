7f4e12da31de8472852dc689d8f303a225484e07
yurong <rong.yu@thoughtworks.com>
2020-08-03 21:19:14 +0800

    feat: complete BowlingGame

---
+++ src/main/java/BowlingGame.java
@@ -0,0 +1,67 @@
+import java.util.Arrays;
+
+public class BowlingGame {
+
+    static char STRIKE = 'X';
+    static char SPARE = '/';
+    static int FULL_SCORE = 10;
+    static int FIRSTTIME = 0;
+    static int SECONDTIME = 1;
+    static int TENROUND = 10;
+    String[] roundScores;
+
+    public BowlingGame(String[] roundScores) {
+        this.roundScores = roundScores;
+    }
+
+    public int getScoreFromTime(char sign){
+        if (sign == STRIKE || sign == SPARE)
+            return FULL_SCORE;
+        return sign - '0';
+    }
+
+    public int getScoreFromRound(String roundScore){
+        if (isStrike(roundScore) || isSpare(roundScore))
+            return FULL_SCORE;
+        return getScoreFromTime(roundScore.charAt(FIRSTTIME)) + getScoreFromTime(roundScore.charAt(SECONDTIME));
+    }
+
+    public int getEachRoundRealSocre(int nowRound){
+        String nowRoundScore = this.roundScores[nowRound - 1];
+        if (isStrike(nowRoundScore) || isSpare(nowRoundScore))
+            return getScoreFromRound(nowRoundScore) + getNextTimeScore(this.roundScores, nowRound, isStrike(nowRoundScore));
+
+        return getScoreFromRound(nowRoundScore);
+    }
+
+    public int getTotalScore(){
+        int totalScore = 0;
+        for (int i = 0; i < 10; i++){
+            totalScore = totalScore + getEachRoundRealSocre(i+1);
+        }
+        return totalScore ;
+
+    }
+    public int getNextTimeScore(String[] roundScores, int nowRound, boolean twoNext){
+        String nextRoundScores = roundScores[nowRound];
+        String twoNextRoundScores = roundScores[nowRound + (nowRound== TENROUND ?0:1)];
+        if (!twoNext)
+            return getScoreFromTime(nextRoundScores.charAt(FIRSTTIME));
+        if (isStrike(nextRoundScores))
+            return getScoreFromRound(nextRoundScores) + getScoreFromTime(twoNextRoundScores.charAt(FIRSTTIME));
+        return getScoreFromRound(nextRoundScores);
+    }
+
+    public boolean isStrike(String roundScore){
+        if (roundScore.length() == 1 && roundScore.charAt(FIRSTTIME) == STRIKE)
+            return true;
+        return false;
+    }
+
+    public boolean isSpare(String roundScore){
+        if (roundScore.length() == 2 && roundScore.charAt(SECONDTIME) == SPARE)
+            return true;
+        return false;
+    }
+
+}
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -1,2 +1,9 @@
+import org.junit.jupiter.api.Test;
+
 public class BowlingGameTest {
+
+    @Test
+    void should_return_300_when_ten_strike(){
+
+    }
 }
